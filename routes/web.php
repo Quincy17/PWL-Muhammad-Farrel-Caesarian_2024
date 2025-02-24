86ff0bb2b284cfcc684db18c03501e9b88d7532e
Quincy17 <farrelcaesarian15@gmail.com>
2025-02-24 16:17:45 +0700

    Add Optional Parameters

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Mengambil daftar hello kemudian menampilkan Hello World di halaman web
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+//Mengambil daftar world kemudian menampilkan World di halaman web
+Route::get('/world', function () {
+    return 'World';
+});
+
+//Route untuk menampilkan selamat datang
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+//Route untuk menampilkan NIM dan Nama Saya
+Route::get('/about', function () {
+    return '2341720053 Muhammad Farrel Caesarian';
+});
+
+//Memanggil Route /user/{name} sekaligus mengirimkan parameter berupa nama user$name
+Route::get('/user/{name}', function($name){
+    return 'Nama Saya '.$name;
+});
+
+//Memanggil Route yang mempunyai lebih dari 2 parameter
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+//Membuat Route articles yang dapat menampilkan halaman artikel dengan id
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+//Membuat Route dengan Optional Parameter
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama Saya '.$name;
 });
